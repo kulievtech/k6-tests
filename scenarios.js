0b2ee087bca3c19215be9aeeb517a4aee34c9c88
Tohir Kuliev <kuliev.tech@gmail.com>
2026-04-11 10:49:38 -0400

    Update

---
+++ scenarios.js
@@ -0,0 +1,15 @@
+import http from "k6/http";
+import { check } from "k6";
+
+export default function () {
+  const res = http.get("https://quickpizza.grafana.com/test.k6.io/");
+
+  // Good practice to check the response status and content to ensure the test is working as expected
+  check(res, {
+    "is status 200": (r) => r.status === 200,
+  });
+
+  check(res, {
+    "page is startpage": (r) => r.body.includes("Public pages"),
+  });
+}
