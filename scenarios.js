68b3aa306381cbcfe02c5d10f5c0ed7990497a85
Tohir Kuliev <kuliev.tech@gmail.com>
2026-04-11 11:06:58 -0400

    Add thresholds

--- scenarios.js
+++ scenarios.js
@@ -1,5 +1,15 @@
 import http from "k6/http";
-import { check } from "k6";
+import { check, sleep } from "k6";
+
+export const options = {
+  vus: 10, // Number of virtual users
+  duration: "10s", // Duration of the test
+  thresholds: {
+    // Define thresholds for performance metrics
+    http_req_duration: ["p(95)<100"], // 95% of requests should complete in under 100ms
+    http_req_failed: ["rate<0.01"], // Fail if more than 1% of requests fail
+  },
+};
 
 export default function () {
   const res = http.get("https://quickpizza.grafana.com/test.k6.io/");
@@ -9,4 +19,6 @@ export default function () {
     "is status 200": (r) => r.status === 200,
     "page is startpage": (r) => r.body.includes("Public pages"),
   });
+
+  sleep(2); // Simulate user think time
 }
